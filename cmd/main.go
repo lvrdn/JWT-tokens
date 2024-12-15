deb0c01a52385b0311e701f8d5b55e2b37368c25
Денис Лавринов <da.lavrinov@yandex.ru>
2024-12-15 13:21:53 +0300

    add srever shutdown

--- cmd/main.go
+++ cmd/main.go
@@ -5,10 +5,14 @@ import (
 	"AuthApp/pkg/auth"
 	"AuthApp/pkg/sender"
 	"AuthApp/pkg/storage"
+	"context"
 	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 
 	_ "github.com/lib/pq"
 )
@@ -40,13 +44,29 @@ func main() {
 	}
 
 	authHandler := &auth.AuthHandler{
-		Storage:     storage.NewStorage(db),
-		Keys:        auth.NewKeys(),
-		EmailSender: sender.NewEmailSenderServer(),
+		AccessKey:        cfg.AccessKey,
+		AccessExpMinutes: cfg.AccessExpMinutes,
+		RefreshExpMonths: cfg.RefreshExpMonths,
+		Storage:          storage.NewStorage(db),
+		EmailSender:      sender.NewEmailSenderServer(),
 	}
 
 	http.HandleFunc("GET /api/auth", authHandler.Issue)
 	http.HandleFunc("GET /api/refresh", authHandler.Refresh)
 
-	http.ListenAndServe(":"+cfg.HTTPport, nil)
+	server := http.Server{
+		Addr: ":" + cfg.HTTPport,
+	}
+
+	go func() {
+		server.ListenAndServe()
+	}()
+
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+
+	<-c
+
+	server.Shutdown(context.Background())
+	fmt.Println("Server stopped")
 }

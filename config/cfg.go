5c4553fca2a80a2cd2632d76424b0faa0d960f80
Денис Лавринов <da.lavrinov@yandex.ru>
2024-12-15 13:24:35 +0300

    add access key and exp to cfg

--- config/cfg.go
+++ config/cfg.go
@@ -1,17 +1,22 @@
 package config
 
 import (
+	"fmt"
 	"os"
+	"strconv"
 
 	"github.com/joho/godotenv"
 )
 
 type Config struct {
-	HTTPport   string
-	DBhost     string
-	DBname     string
-	DBusername string
-	DBpassword string
+	HTTPport         string
+	DBhost           string
+	DBname           string
+	DBusername       string
+	DBpassword       string
+	AccessKey        string
+	AccessExpMinutes int
+	RefreshExpMonths int
 }
 
 func GetConfig() (*Config, error) {
@@ -26,11 +31,24 @@ func GetConfig() (*Config, error) {
 		return nil, err
 	}
 
+	accessExpMinutes, err := strconv.Atoi(env["ACCESS_EXP_MINUTES"])
+	if err != nil {
+		return nil, fmt.Errorf(`error with parse config param "ACCESS_EXP_MINUTES", check config value must be int`)
+	}
+
+	refreshExpMonths, err := strconv.Atoi(env["REFRESH_EXP_MONTHS"])
+	if err != nil {
+		return nil, fmt.Errorf(`error with parse config param "REFRESH_EXP_MONTHS", check config value must be int`)
+	}
+
 	return &Config{
-		HTTPport:   env["HTTP_PORT"],
-		DBhost:     env["DB_HOST"],
-		DBname:     env["DB_NAME"],
-		DBusername: env["DB_USERNAME"],
-		DBpassword: env["DB_PASSWORD"],
+		HTTPport:         env["HTTP_PORT"],
+		DBhost:           env["DB_HOST"],
+		DBname:           env["DB_NAME"],
+		DBusername:       env["DB_USERNAME"],
+		DBpassword:       env["DB_PASSWORD"],
+		AccessKey:        env["ACCESS_KEY"],
+		AccessExpMinutes: accessExpMinutes,
+		RefreshExpMonths: refreshExpMonths,
 	}, nil
 }
